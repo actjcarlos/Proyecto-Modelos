96af62f42c25500a3efa5d68e6102b5f6e40e214
actjcarlos <act.carlos.r@gmail.com>
2025-01-23 23:33:05 -0600

    Add files via upload
    Este es el primero programa en Python del curso de Coursera

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,194 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "fff0b197",
+   "metadata": {},
+   "source": [
+    "**Exercise 2:** "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "87be070c",
+   "metadata": {},
+   "source": [
+    "# DATA SCIENCE ECOSYSTEM"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "83ff6ce3",
+   "metadata": {},
+   "source": [
+    "**Exercise 3:**  \n",
+    "Hi!  \n",
+    "In this notebook, Data Science Tools and Ecosystem are summzarized "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "80b4d1fc",
+   "metadata": {},
+   "source": [
+    "**Exercise 10:**  \n",
+    "**Objetives:**  \n",
+    "* Learn the popular languages in Data Science\n",
+    "* Diference markdown cell than code cell\n",
+    "* How to program the adittion between two numbers"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "1adc5138",
+   "metadata": {},
+   "source": [
+    "**Exercise 4:**  \n",
+    "Some of the popuar laguages that Dara Scientists use are:  \n",
+    "* Python\n",
+    "* R and Rstudio \n",
+    "* AWS\n",
+    "* JULIA\n",
+    "* SQL "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "d8c4e777",
+   "metadata": {},
+   "source": [
+    "**Exercise 5:**  \n",
+    "Some of commonly used libraries used for Data Scientists include:  \n",
+    "* TensorFlow\n",
+    "* Pytorch\n",
+    "* Scikit-learn\n",
+    "* Keras "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "52201a15",
+   "metadata": {},
+   "source": [
+    "**Exercise 6:** \n",
+    "\n",
+    "|DATA SCIENCE TOOL|\n",
+    "|:---------------:|\n",
+    "|Apache Spark     |\n",
+    "|Apache Hadoop    |\n",
+    "|Git              | "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0e6d3cf4",
+   "metadata": {},
+   "source": [
+    "**Exercise 7:**  \n",
+    "### Below are a few examples of evaluating arithmetic expressions in Python.  \n",
+    "\n",
+    "</code>y=2  \n",
+    "x=5  \n",
+    "z=x+y<code>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "ece7bae4",
+   "metadata": {},
+   "source": [
+    "**Exercise 8:**  "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "65611eb6",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "17"
+      ]
+     },
+     "execution_count": 6,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "#This simple arithmetic expression to multiply the add integers\n",
+    "(3*4)+5"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "d0308697",
+   "metadata": {},
+   "source": [
+    "**Exercise 9:**  "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "a4286a04",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "3.3333333333333335"
+      ]
+     },
+     "execution_count": 5,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "#This will convert 200 minutes to hours by diving by 60\n",
+    "200/60"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "2b1cd8e2",
+   "metadata": {},
+   "source": [
+    "**Exercise 11:**  "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "1ede4037",
+   "metadata": {},
+   "source": [
+    "## Author:  \n",
+    "Juan Carlos Ramirez Romero"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
